61913ec7ee358fbcb62181fcc90cbf6eabe3d4a1
ismailzia01 <ismailzia890ad@gmail.com>
2025-10-25 00:55:31 +0530

    coding challaneges word count tool

--- DAA LAB/test.c
+++ DAA LAB/test.c
@@ -1,56 +1,50 @@
 #include<stdio.h>
-
-int parant[10];
-
-int find(int i) {
-    while(parant[i] != i)
-        i = parant[i];
-    return i;
-}
-
-void uni(int i, int j) {
-    int a = find(i);
-    int b = find(j);
-    parant[a] = b;
-}
-
-int main() {
-    int n, e;
-    int s, d, w;
-    int min, total_cost = 0;
-    int cost[10][10];
-    int u , v;
-
-    printf("enter no. of nodes : ");
-    for(int i = 0; i<n; i++) {
-        for(int j = 0; j < n; j++) {
-            scanf("%d", &cost[i][j]);
-            if(cost[i][j]==0)
-                cost[i][j] = 999;
-        }
-    }
+#include<stdlib.h>
+#define N 10
+int profit[N] = {10, 5, 15, 7, 6, 18, 3};
+int weight[N] = {2, 3, 5, 7, 1, 4, 1};
+double pw[N];
+int n = 7, m = 15;
+void sort() {
     for(int i = 0; i < n; i++) {
-        parant[i] = i;
-    }
-    e=0;
-    while(e < n-1) {
-        min = 999;
-        for(int i = 0; i < n; i++) {
-            for(int j = 0; j <n; j++) {
-                if(find(i) != find(j) && cost[i][j] < min){
-                    min = cost[i][j];
-                    s = i;
-                    d = j;
-                }
+        for(int j = 0; j < n - i - 1; j++) {
+            if(pw[j] < pw[j+1]) {
+                double temp3 = pw[j];
+                pw[j] = pw[j+1];
+                pw[j+1] = temp3;
+
+                int temp = profit[j];
+                profit[j] = profit[j+1];
+                profit[j+1] = temp;
+
+                int temp2 = weight[j];
+                weight[j] = weight[j+1];
+                weight[j+1] = temp2;
             }
         }
-    
-        uni(s, d);
-        printf("%d -> %d cost %d", s, d, min);
-        total_cost += min;
-        cost[s][d] = cost[d][s] = 999;
-        e++;
     }
-    printf("MAx profit %d ", total_cost);
+}
+
+double greedyKnapSack() {
+    double max_profit = 0;
+    for(int i = 0; i < n; i++) {
+        if(weight[i] <= m) {
+            max_profit += profit[i];
+            m -= weight[i];
+        }
+        else {
+            max_profit += pw[i] * m;
+            break;
+        }
+    }
+    return max_profit;
+}
+
+
+int main() {
+    for(int i = 0; i < n; i++)
+        pw[i] = (double)profit[i] / weight[i];
+    sort();
+    printf("\nProfit : %f \n", greedyKnapSack());
     return 0;
 }
